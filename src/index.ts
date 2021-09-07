edf0908415bac9053c6aa5ce9db5716c56058320
ALDI <aldi7th@gmail.com>
2021-09-07 12:02:57 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -3,62 +3,75 @@ import prompt from "prompt";
 import optimist from "optimist";
 import cron from "node-cron";
 import fs from "fs";
-import {CHAIN_CONFIG, executeJobByCID} from "flow-call-sdk";
-import {Wallet} from "@ethersproject/wallet";
-import {JsonRpcProvider} from "@ethersproject/providers";
+import { CHAIN_CONFIG, executeJobByCID } from "flow-call-sdk";
+import { Wallet } from "@ethersproject/wallet";
+import { JsonRpcProvider } from "@ethersproject/providers";
+import {
+    bootstrap
+  } from 'global-agent';
+  
+bootstrap();
 
-const FILE_PRIVATE_KEY=".key";
+const FILE_PRIVATE_KEY = ".key";
 
-let jobCid:string;
-let walletKey:string;
+let jobCid: string;
+let walletKey: string;
 
 
-const init=async ()=>{
+const init = async () => {
     try {
         logger.info("Deflow JobRunner is starting...");
 
         let pkByFile;
-        if(fs.existsSync(FILE_PRIVATE_KEY)){
-            pkByFile=fs.readFileSync(FILE_PRIVATE_KEY)?.toString();
+        if (fs.existsSync(FILE_PRIVATE_KEY)) {
+            pkByFile = fs.readFileSync(FILE_PRIVATE_KEY)?.toString();
         }
 
-        prompt.override=optimist.usage("Usage: $0 -c <cid> -k <walletKey>").alias("cid","c").alias("walletKey","k").argv;
+        prompt.override = optimist.usage("Usage: $0 -c <cid> -k <walletKey> -p <proxy>").alias("cid", "c").alias("walletKey", "k").alias("proxy","p").argv;
+        const proxy=global.GLOBAL_AGENT.HTTP_PROXY;
         prompt.start();
-        const inputs=await prompt.get([
+        const inputs = await prompt.get([
             {
-                name:"cid",
-                description:"Input the IPFS CID of the job to run",
-                required:true
-            },{
-                name:"walletKey",
-                description:`Paste your wallet's private key or mnemonic phrase here${pkByFile?"(press ENTER to keep using current key)":""})`,
-                required:!pkByFile
+                name: "cid",
+                description: "Input the IPFS CID of the job to run",
+                required: true
+            }, {
+                name: "walletKey",
+                description: `Paste your wallet's private key or mnemonic phrase here${pkByFile ? "(press ENTER to keep using current key)" : ""})`,
+                required: !pkByFile
+            }, {
+                name: "proxy",
+                description: `Set HTTP proxy server(${proxy?proxy:"ignore"})`,
+                required:false
             }
         ]);
-        jobCid=inputs.cid as string;
+        if(inputs.proxy){
+            global.GLOBAL_AGENT.HTTP_PROXY=inputs.proxy;
+        }
+        jobCid = inputs.cid as string;
         logger.info(`Got CID: ${jobCid}`);
-        walletKey=inputs.walletKey as string;
-        if(walletKey){
-            fs.writeFileSync(FILE_PRIVATE_KEY,walletKey);
+        walletKey = inputs.walletKey as string;
+        if (walletKey) {
+            fs.writeFileSync(FILE_PRIVATE_KEY, walletKey);
             logger.info(`Your wallet key was saved in file '${FILE_PRIVATE_KEY}' and will ONLY be used locally.`);
         }
-        if(!walletKey && pkByFile){
-            walletKey=pkByFile;
+        if (!walletKey && pkByFile) {
+            walletKey = pkByFile;
         }
-        if(!walletKey){
+        if (!walletKey) {
             logger.error("")
-        }   
-        
-        const job=await executeJobByCID(jobCid);
-        cron.schedule(job.cron,async ()=>{
+        }
+
+        const job = await executeJobByCID(jobCid);
+        cron.schedule(job.cron, async () => {
             logger.info("Start executing...");
-            try{
-                const provider=new JsonRpcProvider(CHAIN_CONFIG[job.chainId].rpcUrl);
-                const wallet=new Wallet(walletKey,provider);
+            try {
+                const provider = new JsonRpcProvider(CHAIN_CONFIG[job.chainId].rpcUrl);
+                const wallet = new Wallet(walletKey, provider);
                 await job.worker.execute(wallet);
                 logger.info("Job executed!!!");
             }
-            catch(e){
+            catch (e) {
                 logger.error(e);
             }
         });
@@ -67,9 +80,12 @@ const init=async ()=>{
         logger.info(`Deflow JobRunner for [${jobCid}] started.`);
     } catch (e) {
         logger.error(e);
-        process.exit();
+        console.log('Press any key to exit');
+        process.stdin.setRawMode(true);
+        process.stdin.resume();
+        process.stdin.on('data', process.exit.bind(process, 0));
     }
-    
+
 };
 
-init(); 
+init();
